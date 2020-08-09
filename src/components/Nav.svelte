7eef3889f7e749b78dd6c6c85d819860214ff3ab
Levy Adams <letitslide87@gmail.com>
2020-08-09 19:48:49 -0400

    fe update

---
+++ src/components/Nav.svelte
@@ -0,0 +1,63 @@
+<script>
+	export let segment;
+</script>
+
+<style>
+	nav {
+		font-weight: 300;
+		padding: 0 1em;
+		position: fixed;
+		bottom:0;
+		width: 100%;
+		
+	}
+
+	ul {
+		margin: 0;
+		padding: 0;
+		display:flex;
+	}
+
+	/* clearfix */
+	ul::after {
+		content: '';
+		display: block;
+		clear: both;
+	}
+
+	li {
+		display: block;
+		width:50%;
+	}
+
+	[aria-current] {
+		position: relative;
+		display: inline-block;
+	}
+
+	[aria-current]::after {
+		position: absolute;
+		content: '';
+		width: calc(100% - 1em);
+		height: 2px;
+		background-color: rgb(255,62,0);
+		display: block;
+		bottom: -1px;
+	}
+
+	a {
+		text-decoration: none;
+		padding: 1em 0.5em;
+		display: block;
+	}
+</style>
+
+<nav>
+	<ul>
+		<li><a aria-current="{segment === undefined ? 'page' : undefined}" rel=prefetch href=".">games</a></li>
+		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">updates</a></li>
+
+		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+		     the blog data when we hover over the link or tap it on a touchscreen -->
+	</ul>
+</nav>
